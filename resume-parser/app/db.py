d71c5d1da5178ae8bf87e2061f09d665022564b0
xeres <xeres@xeress-MacBook-Air.local>
2025-07-25 13:42:09 +0530

    fix

--- resume-parser/app/db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-import os
-
-POSTGRES_URL = os.getenv("POSTGRES_URL", "postgresql://user:password@postgres:5432/resumedb")
-
-engine = create_engine(POSTGRES_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
