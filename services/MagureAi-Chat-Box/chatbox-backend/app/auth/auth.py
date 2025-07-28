8e90e8e2b6f5d61035ef12a52101ad54e1233761
Ashutosh975537 <ashutoshgirigoswami525@gmail.com>
2025-07-28 18:57:02 +0530

    login issue fixed

--- services/MagureAi-Chat-Box/chatbox-backend/app/auth/auth.py
+++ services/MagureAi-Chat-Box/chatbox-backend/app/auth/auth.py
@@ -5,19 +5,10 @@ from .. import schemas, crud
 from ..utils import create_access_token,verify_password,get_password_hash
 
 
-def login_user(user: schemas.UserLogin, db: Session):
-    
-        
+def login_user(user: schemas.UserLogin, db: Session):    
     db_user = crud.get_user_by_username(user.username, db)
-    
 
-    verified_password = verify_password(user.password,db_user.password)    
-    
-    
-    print("<============== The verified passward is ============>" , verified_password)
-    
-    
-    if not db_user or not verified_password:
+    if not db_user or not verify_password(user.password,db_user.password):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     
     token = create_access_token({"sub": str(db_user.id)})
